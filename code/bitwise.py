ae4bc34052881bafff705fb49d56bc97c4fe3ad3
Jacob Castanon <jacastanon01@gmail.com>
2024-05-15 16:34:46 -0500

    Fun with binary

--- code/bitwise.py
+++ code/bitwise.py
@@ -30,7 +30,7 @@ allergies = [
 
 
 class Allergies:
-    def __init__(self, score):
+    def __init__(self, score) -> None:
         self.allergies = [
             allergin
             for i, allergin in enumerate(allergies)  # loop through allergies with index
@@ -39,40 +39,9 @@ class Allergies:
             # allergies[i] matches the new number derived from bitwise &
         ]
 
-    def allergic_to(self, item):
+    def allergic_to(self, item) -> bool:
         return item in self.allergies
 
     @property
-    def lst(self):
-        return self.allergies
-
-
-# Defining the allergies
-allergies = [
-    "eggs",
-    "peanuts",
-    "shellfish",
-    "strawberries",
-    "tomatoes",
-    "chocolate",
-    "pollen",
-    "cats",
-]
-
-
-class Allergies:
-    def __init__(self, score):
-        self.allergies = [
-            allergin
-            for i, allergin in enumerate(allergies)  # loop through allergies with index
-            if score & (1 << i)  # shift 1 left i times and compare each bit to score
-            # if any bit has a 1 in its place, it will be true and the allergy and
-            # allergies[i] matches the new number derived from bitwise &
-        ]
-
-    def allergic_to(self, item):
-        return item in self.allergies
-
-    @property
-    def lst(self):
+    def lst(self) -> list[str]:
         return self.allergies
