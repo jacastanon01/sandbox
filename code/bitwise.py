5fe15db0fe70367b5e369cbb0c0530f99bcabd00
Jacob Castanon <jacastanon01@gmail.com>
2024-05-15 16:26:25 -0500

    fun with binary

--- code/bitwise.py
+++ code/bitwise.py
@@ -15,3 +15,64 @@ dict3 = {"a": 1, "b": 5}
 
 print("dict1 | dict2\nUnion", (dict2 | dict3), sep=": ")
 # print("dict1 & dict2\nIntersection: ", (dict2 & dict2), sep=": ")
+
+# Defining the allergies
+allergies = [
+    "eggs",
+    "peanuts",
+    "shellfish",
+    "strawberries",
+    "tomatoes",
+    "chocolate",
+    "pollen",
+    "cats",
+]
+
+
+class Allergies:
+    def __init__(self, score):
+        self.allergies = [
+            allergin
+            for i, allergin in enumerate(allergies)  # loop through allergies with index
+            if score & (1 << i)  # shift 1 left i times and compare each bit to score
+            # if any bit has a 1 in its place, it will be true and the allergy and
+            # allergies[i] matches the new number derived from bitwise &
+        ]
+
+    def allergic_to(self, item):
+        return item in self.allergies
+
+    @property
+    def lst(self):
+        return self.allergies
+
+
+# Defining the allergies
+allergies = [
+    "eggs",
+    "peanuts",
+    "shellfish",
+    "strawberries",
+    "tomatoes",
+    "chocolate",
+    "pollen",
+    "cats",
+]
+
+
+class Allergies:
+    def __init__(self, score):
+        self.allergies = [
+            allergin
+            for i, allergin in enumerate(allergies)  # loop through allergies with index
+            if score & (1 << i)  # shift 1 left i times and compare each bit to score
+            # if any bit has a 1 in its place, it will be true and the allergy and
+            # allergies[i] matches the new number derived from bitwise &
+        ]
+
+    def allergic_to(self, item):
+        return item in self.allergies
+
+    @property
+    def lst(self):
+        return self.allergies
